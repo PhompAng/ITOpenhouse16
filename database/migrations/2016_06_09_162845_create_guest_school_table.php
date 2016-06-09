29af4580032efb9d0f303eab86d15c0c962066ca
tharamalai <57070054@kmitl.ac.th>
2016-06-10 01:32:45 +0700

    add migration of guest table

---
+++ database/migrations/2016_06_09_162845_create_guest_school_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGuestSchoolTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guest_school', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('prefix');
+            $table->text('name');
+            $table->text('surname');
+            $table->enum('gender',['M', 'F', 'U']);
+            $table->tinyInteger('age')->nullable();
+            $table->text('school');
+            $table->smallInteger('follower');
+            $table->text('province')->nullable();
+            $table->text('phone')->nullable();
+            $table->text('email');
+            $table->text('facebook')->nullable();
+            $table->text('twittter')->nullable();
+            $table->tinyInteger('news')->default(0);
+            $table->timestamp('confirm')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('guest_school');
+    }
+}

4308f1267d6eed21673adc8de99ee403d75b62b5
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-07-04 11:24:08 +0700

    networkController@store

--- app/Http/Controllers/Competition/NetworkController.php
+++ app/Http/Controllers/Competition/NetworkController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Competition;
 
+use App\Models\Network;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests;
+use Illuminate\Support\Facades\Validator;
 
 class NetworkController extends Controller
 {
@@ -36,7 +38,71 @@ class NetworkController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $inputs = $request->all();
+
+        $rules = [
+            'team_name'             => 'required',
+            'rank'             => 'required|integer|between:1,100',
+            'name.*'				=> 'required',
+            'surname.*'             => 'required',
+            'email.*'               => 'required|email',
+            'phone.*'               => 'required|regex:/^0[0-9]{1,2}[0-9]{7}$/',
+            'school'				=> 'required',
+            'school_addr'			=> 'required',
+            'school_province'		=> 'required',
+            'teacher_prefix'        => 'required',
+            'teacher_name'		    => 'required',
+            'teacher_surname'		=> 'required',
+            'teacher_email'         => 'required|email',
+            'teacher_phone'			=> 'required|regex:/^0[0-9]{1,2}[0-9]{7}$/'
+        ];
+
+        $messages = [
+            'team_name.required'             =>  'กรุณากรอก  ชื่อทีม',
+            'rank.required'             =>  'กรุณากรอก  อันดับของทีม',
+            'name.*.required'                =>  'กรุณากรอก  ชื่อสมาชิก',
+            'surname.*.required'             =>  'กรุณากรอก  นามสกุลของสมาชิก',
+            'email.*.required'               =>  'กรุณากรอก  อีเมลของสมาชิก',
+            'phone.*.required'               =>  'กรุณากรอก  เบอร์โทรศัพท์ของสมาชิก',
+            'school.required'                =>  'กรุณากรอก  ชื่อโรงเรียน',
+            'school_addr.required'           =>  'กรุณากรอก  ที่อยู่โรงเรียน',
+            'school_province.required'       =>  'กรุณากรอก  จังหวัด',
+            'teacher_prefix.required'        =>  'กรุณากรอก  คำนำหน้าชื่ออาจารย์ผู้ควบคุมทีม',
+            'teacher_name.required'          =>  'กรุณากรอก  ชื่ออาจารย์ผู้ควบคุมทีม',
+            'teacher_surname.required'       =>  'กรุณากรอก  นามสกุลอาจารย์ผู้ควบคุมทีม',
+            'teacher_phone.required'         =>  'กรุณากรอก  เบอร์โทรศัพท์อาจารย์ผู้ควบคุมทีม',
+            'teacher_email.required'         =>  'กรุณากรอก  อีเมลอาจารย์ผู้ควบคุมทีม',
+            'team_rank.integer'              =>  'รูปแบบ  อันดับทีม ไม่ถูกต้อง',
+            'team_rank.between'              =>  'อันดับทีมต้องอยู่ระหว่าง 1 ถึง 100',
+            'email.*.email'                  =>  'รูปแบบ  อีเมลสมาชิก ไม่ถูกต้อง',
+            'phone.*.regex'                  =>  'รูปแบบ  เบอร์โทรศัพท์สมาชิก ไม่ถูกต้อง',
+            'teacher_email.email'            =>  'รูปแบบ  อีเมลอาจารย์ผู้ควบคุมทีม ไม่ถูกต้อง',
+            'teacher_phone.regex'            =>  'รูปแบบ  เบอร์โทรศัพท์อาจารย์ผู้ควบคุมทีม ไม่ถูกต้อง'
+        ];
+
+        $validator = Validator::make($inputs, $rules, $messages);
+        if($validator->fails()){
+            return redirect('/register/competition/network/create')->withInput()->withErrors($validator);
+        }
+
+        $network = new Network();
+        $network->fill($request->all());
+
+        $members = [];
+        for ($i = 0; $i < 2; $i++){
+            $member['prefix'] = $inputs['prefix'][$i];
+            $member['name'] = $inputs['name'][$i];
+            $member['surname'] = $inputs['surname'][$i];
+            $member['class'] = $inputs['class'][$i];
+            $member['email'] = $inputs['email'][$i];
+            $member['phone'] = $inputs['phone'][$i];
+            $members[] = $member;
+        }
+
+        $network->member = json_encode($members, JSON_UNESCAPED_UNICODE);
+        $network->save();
+
+        return view('register.competition.network.create', ['success' => 1]);
     }
 
     /**

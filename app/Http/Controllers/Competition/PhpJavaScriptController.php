a7f03d4181f9b53d1aaef520ae46f39ce7e39a88
tharamalai <57070054@kmitl.ac.th>
2016-07-04 13:47:37 +0700

    add PhpJavaScript Register

--- app/Http/Controllers/Competition/PhpJavaScriptController.php
+++ app/Http/Controllers/Competition/PhpJavaScriptController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Competition;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Php;
 
 class PhpJavaScriptController extends Controller
 {
@@ -36,7 +38,63 @@ class PhpJavaScriptController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $inputs = $request->all();
+
+        $rules = [
+            'team_name'             => 'required',
+            'name.*'				=> 'required',
+            'surname.*'             => 'required',
+            'email.*'               => 'required|email',
+            'school'				=> 'required',
+            'school_addr'			=> 'required',
+            'school_province'		=> 'required',
+            'teacher_prefix'        => 'required',
+            'teacher_name'		    => 'required',
+            'teacher_surname'		=> 'required',
+            'teacher_email'         => 'required|email',
+            'teacher_phone'			=> 'required|regex:/^0[0-9]{1,2}[0-9]{7}$/'
+        ];
+
+        $messages = [
+            'team_name.required'             =>  'กรุณากรอก  ชื่อทีม',
+            'name.*.required'                =>  'กรุณากรอก  ชื่อสมาชิก',
+            'surname.*.required'             =>  'กรุณากรอก  นามสกุลของสมาชิก',
+            'email.*.required'               =>  'กรุณากรอก  อีเมลของสมาชิก',
+            'school.required'                =>  'กรุณากรอก  ชื่อโรงเรียน',
+            'school_addr.required'           =>  'กรุณากรอก  ที่อยู่โรงเรียน',
+            'school_province.required'       =>  'กรุณากรอก  จังหวัด',
+            'teacher_prefix.required'        =>  'กรุณากรอก  คำนำหน้าชื่ออาจารย์ผู้ควบคุมทีม',
+            'teacher_name.required'          =>  'กรุณากรอก  ชื่ออาจารย์ผู้ควบคุมทีม',
+            'teacher_surname.required'       =>  'กรุณากรอก  นามสกุลอาจารย์ผู้ควบคุมทีม',
+            'teacher_phone.required'         =>  'กรุณากรอก  เบอร์โทรศัพท์อาจารย์ผู้ควบคุมทีม',
+            'teacher_email.required'            =>  'กรุณากรอก  อีเมลอาจารย์ผู้ควบคุมทีม',
+            'email.*.email'                  =>  'รูปแบบ  อีเมลสมาชิก ไม่ถูกต้อง',
+            'teacher_email.email'            =>  'รูปแบบ  อีเมลอาจารย์ผู้ควบคุมทีม ไม่ถูกต้อง',
+            'teacher_phone.regex'            =>  'รูปแบบ  เบอร์โทรศัพท์อาจารย์ผู้ควบคุมทีม ไม่ถูกต้อง'
+        ];
+
+        $validator = Validator::make($inputs, $rules, $messages);
+        if($validator->fails()){
+            return redirect('/register/competition/php/create')->withInput()->withErrors($validator);
+        }
+
+        $php = new Php();
+        $php->fill($request->all());
+
+        $members = [];
+        for ($i = 0; $i < 2; $i++){
+            $member['prefix'] = $inputs['prefix'][$i];
+            $member['name'] = $inputs['name'][$i];
+            $member['surname'] = $inputs['surname'][$i];
+            $member['class'] = $inputs['class'][$i];
+            $member['email'] = $inputs['email'][$i];
+            $members[] = $member;
+        }
+
+        $php->member = json_encode($members, JSON_UNESCAPED_UNICODE);
+        $php->save();
+
+        return view('register.competition.php.create', ['success' => 1]);
     }
 
     /**

9de82d31170f935cd0c904975eeabd7005f78378
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-07-04 07:05:16 +0700

    ITQuizController@store

--- app/Http/Controllers/Competition/ITQuizController.php
+++ app/Http/Controllers/Competition/ITQuizController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Competition;
 
+use App\Models\ITQuiz;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests;
+use Illuminate\Support\Facades\Validator;
 
 class ITQuizController extends Controller
 {
@@ -36,7 +38,63 @@ class ITQuizController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $inputs = $request->all();
+
+        $rules = [
+            'team_name'             => 'required',
+            'name.*'				=> 'required',
+            'surname.*'             => 'required',
+            'email.*'               => 'required|email',
+            'school'				=> 'required',
+            'school_addr'			=> 'required',
+            'school_province'		=> 'required',
+            'teacher_prefix'        => 'required',
+            'teacher_name'		    => 'required',
+            'teacher_surname'		=> 'required',
+            'teacher_email'         => 'required|email',
+            'teacher_phone'			=> 'required|regex:/^0[0-9]{1,2}[0-9]{7}$/'
+        ];
+
+        $messages = [
+            'team_name.required'             =>  'กรุณากรอก  ชื่อทีม',
+            'name.*.required'                =>  'กรุณากรอก  ชื่อสมาชิก',
+            'surname.*.required'             =>  'กรุณากรอก  นามสกุลของสมาชิก',
+            'email.*.required'               =>  'กรุณากรอก  อีเมลของสมาชิก',
+            'school.required'                =>  'กรุณากรอก  ชื่อโรงเรียน',
+            'school_addr.required'           =>  'กรุณากรอก  ที่อยู่โรงเรียน',
+            'school_province.required'       =>  'กรุณากรอก  จังหวัด',
+            'teacher_prefix.required'        =>  'กรุณากรอก  คำนำหน้าชื่ออาจารย์ผู้ควบคุมทีม',
+            'teacher_name.required'          =>  'กรุณากรอก  ชื่ออาจารย์ผู้ควบคุมทีม',
+            'teacher_surname.required'       =>  'กรุณากรอก  นามสกุลอาจารย์ผู้ควบคุมทีม',
+            'teacher_phone.required'         =>  'กรุณากรอก  เบอร์โทรศัพท์อาจารย์ผู้ควบคุมทีม',
+            'teacher_email.email'            =>  'กรุณากรอก  อีเมลอาจารย์ผู้ควบคุมทีม',
+            'email.*.email'                  =>  'รูปแบบ  อีเมลสมาชิก ไม่ถูกต้อง',
+            'teacher_email.email'            =>  'รูปแบบ  อีเมลอาจารย์ผู้ควบคุมทีม ไม่ถูกต้อง',
+            'teacher_phone.regex'            =>  'รูปแบบ  เบอร์โทรศัพท์อาจารย์ผู้ควบคุมทีม ไม่ถูกต้อง'
+        ];
+
+        $validator = Validator::make($inputs, $rules, $messages);
+        if($validator->fails()){
+            return redirect('/register/competition/itquiz/create')->withInput()->withErrors($validator);
+        }
+
+        $quiz = new ITQuiz();
+        $quiz->fill($request->all());
+
+        $members = [];
+        for ($i = 0; $i < 2; $i++){
+            $member['prefix'] = $inputs['prefix'][$i];
+            $member['name'] = $inputs['name'][$i];
+            $member['surname'] = $inputs['surname'][$i];
+            $member['class'] = $inputs['class'][$i];
+            $member['email'] = $inputs['email'][$i];
+            $members[] = $member;
+        }
+
+        $quiz->member = json_encode($members, JSON_UNESCAPED_UNICODE);
+        $quiz->save();
+
+        return view('register.competition.quiz.create', ['success' => 1]);
     }
 
     /**

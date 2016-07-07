ac5eb094c5c74088b5adc917ce7c45776c30e0d1
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-07-07 21:06:38 +0700

    update link

--- resources/views/register/regis_page.blade.php
+++ resources/views/register/regis_page.blade.php
@@ -26,7 +26,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">
+            <a class="navbar-brand" href="/">
                 <img src="{{ URL::asset('assets/image/color.png') }}" class="logo">
             </a>
         </div>

6c9894cd19e250d2fa61737cafc8a0c0237deea7
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-07-09 20:50:20 +0700

    fix composer ci

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ ssh root@dev.iton.in.th <<-'ENDCMD'
     cd /var/www/openhouse16.dev.iton.in.th
     git pull
     composer install
+    composer update
     php artisan migrate --seed --force
 ENDCMD

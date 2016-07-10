4e73cc74631159d502b4d4f5b1729b2546712b51
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-07-10 17:16:12 +0700

    update schedule style

--- resources/views/main/schedule.blade.php
+++ resources/views/main/schedule.blade.php
@@ -4,30 +4,34 @@
         <div class="title">
             <h1>กำหนดการและกิจกรรม</h1>
         </div>
-        <div class="content">
-            <h2 class="text-center">กำหนดการ IT Ladkrabang Open House 2016</h2>
-            <h3 class="text-center">
-                <small>วันศุกร์ที่ 26 สิงหาคม พ.ศ. 2559</small>
-            </h3>
+        <div class="row">
+            <div class="col-xs-12 col-md-10 col-md-offset-1">
+                <div class="content">
+                    <h2 class="text-center">กำหนดการ IT Ladkrabang Open House 2016</h2>
+                    <h3 class="text-center">
+                        <small>วันศุกร์ที่ 26 สิงหาคม พ.ศ. 2559</small>
+                    </h3>
 
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>กิจกรรม</th>
-                        <th>รายละเอียด</th>
-                        <th>เวลา</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>นิทรรศการทางวิชาการ</td>
-                        <td>นิทรรศการแสดงผลงานนักศึกษา และให้ความรู้ทางด้านเทคโนโลยีสารสนเทศ<br>
-                            <b>กลุ่มเป้าหมาย:</b> นักเรียน นักศึกษา และประชาชนผู้สนใจทั่วไป<br>
-                            <b>สถานที่:</b> บริเวณห้องแสดงผลงานชั้น 1 และภายในอาคารคณะเทคโนโลยีสารสนเทศ</td>
-                        <td>09:00 – 16:30 น.</td>
-                    </tr>
-                </tbody>
-            </table>
+                    <table class="table table-bordered">
+                        <thead>
+                        <tr>
+                            <th>กิจกรรม</th>
+                            <th>รายละเอียด</th>
+                            <th>เวลา</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>นิทรรศการทางวิชาการ</td>
+                            <td>นิทรรศการแสดงผลงานนักศึกษา และให้ความรู้ทางด้านเทคโนโลยีสารสนเทศ<br>
+                                <b>กลุ่มเป้าหมาย:</b> นักเรียน นักศึกษา และประชาชนผู้สนใจทั่วไป<br>
+                                <b>สถานที่:</b> บริเวณห้องแสดงผลงานชั้น 1 และภายในอาคารคณะเทคโนโลยีสารสนเทศ</td>
+                            <td>09:00 – 16:30 น.</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 

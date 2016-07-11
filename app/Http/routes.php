1fac1f077938f31fd559669cc5669cf186c3d508
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-07-11 16:02:47 +0700

    add soon page & disable regis guest

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,36 +19,40 @@ Route::get('/', function () {
 
 /*---Test---*/
 Route::get('/test', function (){
-    return view('register.competition.esport.pdf2');
+    return view('soon', ["title" => ""]);
 });
 
-//Guest Register
-Route::get('/register/guest',[
-    'as' => 'createRegisterGuest',
-    'uses' => 'Guest\RegisterController@createGuestRegister'
-]);
-Route::post('/register/guest',[
-    'as' => 'storeRegisterGuest',
-    'uses' => 'Guest\RegisterController@storeGuestRegister'
-]);
-
-Route::get('/register/guest_student',[
-    'as' => 'createGuestStudentRegister',
-    'uses' => 'Guest\RegisterController@createGuestStudentRegister'
-]);
-Route::post('/register/guest_student',[
-    'as' => 'storeGuestStudentRegister',
-    'uses' => 'Guest\RegisterController@storeGuestStudentRegister'
-]);
-
-Route::get('/register/guest_school',[
-    'as' => 'createGuestSchoolRegister',
-    'uses' => 'Guest\RegisterController@createGuestSchoolRegister'
-]);
-Route::post('/register/guest_school',[
-    'as' => 'storeGuestSchoolRegister',
-    'uses' => 'Guest\RegisterController@storeGuestSchoolRegister'
-]);
+Route::any('/register/{any}', function () {
+    return view('soon', ["title" => ""]);
+});
+//
+////Guest Register
+//Route::get('/register/guest',[
+//    'as' => 'createRegisterGuest',
+//    'uses' => 'Guest\RegisterController@createGuestRegister'
+//]);
+//Route::post('/register/guest',[
+//    'as' => 'storeRegisterGuest',
+//    'uses' => 'Guest\RegisterController@storeGuestRegister'
+//]);
+//
+//Route::get('/register/guest_student',[
+//    'as' => 'createGuestStudentRegister',
+//    'uses' => 'Guest\RegisterController@createGuestStudentRegister'
+//]);
+//Route::post('/register/guest_student',[
+//    'as' => 'storeGuestStudentRegister',
+//    'uses' => 'Guest\RegisterController@storeGuestStudentRegister'
+//]);
+//
+//Route::get('/register/guest_school',[
+//    'as' => 'createGuestSchoolRegister',
+//    'uses' => 'Guest\RegisterController@createGuestSchoolRegister'
+//]);
+//Route::post('/register/guest_school',[
+//    'as' => 'storeGuestSchoolRegister',
+//    'uses' => 'Guest\RegisterController@storeGuestSchoolRegister'
+//]);
 
 //Route Resource Competition
 Route::resource('/register/competition/itquiz', 'Competition\ITQuizController');
---
+++ resources/views/soon.blade.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ $title }}</title>
+
+    <!--Bootstrap-->
+    <link href="{{URL::asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/main.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/font-awesome.min.css') }}">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body class="soon">
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container shadow">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-nav">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">
+                <img src="{{ URL::asset('assets/image/color.png') }}" class="logo">
+            </a>
+        </div>
+        <div class="collapse navbar-collapse" id="main-nav">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="{{URL('/')}}">หน้าแรก</a>
+                </li>
+                <li>
+                    <a href="{{URL('/schedule')}}">กำหนดการและกิจกรรม</a>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        การแข่งขัน <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li>
+                            <a href="{{URL('/register/competition/esport')}}">
+
+                                การแข่งขันกีฬาอิเล็กทรอนิกส์
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{URL('/register/competition/itquiz')}}">
+
+                                การแข่งขันตอบปัญหา
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{URL('/register/competition/php')}}">
+
+                                การแข่งขันพัฒนาเว็บไซต์
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{URL('/register/competition/network')}}">
+
+                                การแข่งขันความปลอดภัยของระบบคอมพิวเตอร์
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{URL('/register/competition/pitching')}}">
+
+                                การแข่งขันการนำเสนอแผนธุรกิจ
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        การเดินทาง <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li>
+                            <a href="{{URL('/route/car')}}">
+
+                                การเดินทางโดยรถยนต์
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{URL('/route/bus')}}">
+
+                                การเดินทางโดยรถประจำทาง
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{URL('/route/train')}}">
+
+                                การเดินทางโดยรถไฟ/รถไฟฟ้า
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="{{URL('/contact')}}">ติดต่อสอบถาม</a>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+
+<div class="main">
+</div>
+
+<footer>
+    <div class="container footer shadow --invert">
+        <div class="credit-container">
+            <span>Faculty of Information Technology at KMITL © 2016</span>
+            <span class="pull-right">
+                Open House 2011 | 2012 | 2013 | 2014 | 2016 Developers
+                <a href="#" class="back-to-top">
+                    <i class="fa fa-chevron-up"></i>
+                </a>
+            </span>
+        </div>
+    </div>
+</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="{{URL::asset('assets/js/bootstrap.min.js')}}"></script>
+</body>
+</html>

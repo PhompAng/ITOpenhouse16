c98f700952c8dcbfe39e7f14e26b2f122cb4054c
Tonsai S <t@iton.in.th>
2016-07-12 23:05:22 +0700

    Revert "Adds Open Graph's image tag"
    This reverts commit d976a0a4d5d145b52b328a7c6cabfd6aa289ab10

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title }}IT Ladkrabang Open House 2016</title>
-    <meta property="og:image" content="please add" />
 
     <!--Bootstrap-->
     <link href="{{URL::asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">

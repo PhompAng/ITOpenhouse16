7fdafa9e368da3022f43db221b770d2994350306
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-07-15 15:47:05 +0700

    enable guest regis

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,37 +22,37 @@ Route::get('/test', function (){
     return view('soon', ["title" => ""]);
 });
 
-Route::any('/register/{any}', function () {
-    return view('soon', ["title" => ""]);
-});
+//Route::any('/register/{any}', function () {
+//    return view('soon', ["title" => ""]);
+//});
 //
-////Guest Register
-//Route::get('/register/guest',[
-//    'as' => 'createRegisterGuest',
-//    'uses' => 'Guest\RegisterController@createGuestRegister'
-//]);
-//Route::post('/register/guest',[
-//    'as' => 'storeRegisterGuest',
-//    'uses' => 'Guest\RegisterController@storeGuestRegister'
-//]);
-//
-//Route::get('/register/guest_student',[
-//    'as' => 'createGuestStudentRegister',
-//    'uses' => 'Guest\RegisterController@createGuestStudentRegister'
-//]);
-//Route::post('/register/guest_student',[
-//    'as' => 'storeGuestStudentRegister',
-//    'uses' => 'Guest\RegisterController@storeGuestStudentRegister'
-//]);
-//
-//Route::get('/register/guest_school',[
-//    'as' => 'createGuestSchoolRegister',
-//    'uses' => 'Guest\RegisterController@createGuestSchoolRegister'
-//]);
-//Route::post('/register/guest_school',[
-//    'as' => 'storeGuestSchoolRegister',
-//    'uses' => 'Guest\RegisterController@storeGuestSchoolRegister'
-//]);
+//Guest Register
+Route::get('/register/guest',[
+    'as' => 'createRegisterGuest',
+    'uses' => 'Guest\RegisterController@createGuestRegister'
+]);
+Route::post('/register/guest',[
+    'as' => 'storeRegisterGuest',
+    'uses' => 'Guest\RegisterController@storeGuestRegister'
+]);
+
+Route::get('/register/guest_student',[
+    'as' => 'createGuestStudentRegister',
+    'uses' => 'Guest\RegisterController@createGuestStudentRegister'
+]);
+Route::post('/register/guest_student',[
+    'as' => 'storeGuestStudentRegister',
+    'uses' => 'Guest\RegisterController@storeGuestStudentRegister'
+]);
+
+Route::get('/register/guest_school',[
+    'as' => 'createGuestSchoolRegister',
+    'uses' => 'Guest\RegisterController@createGuestSchoolRegister'
+]);
+Route::post('/register/guest_school',[
+    'as' => 'storeGuestSchoolRegister',
+    'uses' => 'Guest\RegisterController@storeGuestSchoolRegister'
+]);
 
 //Route Resource Competition
 Route::resource('/register/competition/itquiz', 'Competition\ITQuizController');

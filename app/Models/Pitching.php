e0c56d6b93d12e69a3c44da31d008e19934cbd56
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-07-20 14:50:26 +0700

    add member Accessor

--- app/Models/Pitching.php
+++ app/Models/Pitching.php
@@ -10,4 +10,8 @@ class Pitching extends Model
 
     protected $fillable = ['team_name', 'school', 'teacher_prefix',  'teacher_name', 'teacher_surname',
                             'teacher_phone', 'teacher_email', 'idea', 'idea_desc', 'bizcanvas', 'storyboard'];
+
+    public function getMemberAttribute($value) {
+        return json_decode($value, true);
+    }
 }

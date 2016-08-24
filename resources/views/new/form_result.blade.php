e2cd71b9385fa7b88b15f6852992663908a0685a
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-08-24 23:07:33 +0700

    fix form submit message

--- resources/views/new/form_result.blade.php
+++ resources/views/new/form_result.blade.php
@@ -5,6 +5,7 @@
             <h1 class="light text-center">ขอขอบคุณที่ให้ความร่วมมือในการตอบแบบสอบถาม</h1>
             <hr>
             <h3 class="light text-center">รหัสรับของที่ระลึกคือ <strong>{{$code}}</strong></h3>
+            <h3 class="light text-center">ท่านสามารถรับของที่ระลึกได้ที่จุดรับของที่ระลึก</h3>
         </div>
     </div>
 @endsection
